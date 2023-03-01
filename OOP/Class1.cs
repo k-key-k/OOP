e4f5a6b6db142cf6ff41f1061cf1923add329217
Admin <Admin@DESKTOP-Q55SDVG>
2023-03-01 23:03:38 +0300

    Добавьте файлы проекта.

---
+++ OOP/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Person
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public DateTime BDay { get; set; }
+        private char sex;
+        public char Sex
+        {
+            get { return sex; }
+            set { if (value == 'М' | value == 'Ж') sex = value; }
+        }
+        public Person() { }
+
+        public override string ToString()
+        {
+            return Name + " " + Surname + " " + BDay + " " + sex;
+        }
+        public void Read()
+        {
+            Console.WriteLine("Введите имя: ");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            Surname = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения (гггг-мм-дд): ");
+            BDay = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Введите пол(М/Ж): ");
+            sex = char.Parse(Console.ReadLine());
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {Name} Фамилия: {Surname} Возраст: {Age(BDay)} Пол: {sex}");
+        }
+        public int Age(DateTime BDay)
+        {
+            int age = DateTime.Now.Year - BDay.Year;
+            if (DateTime.Now.Day < BDay.Day)
+            {
+                if (DateTime.Now.Month < BDay.Month)
+                {
+                    age--;
+                }
+            }
+            return age;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Person[] people = new Person[n];
+            for (int i = 0; i < n; i++)
+            {
+                people[i] = new Person();
+                people[i].Read();
+            }
+            for (int i = 0; i < people.Length;i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine(people[i].Name);
+                Console.WriteLine(people[i].Surname);
+                Console.WriteLine(people[i].Age(people[i].BDay));
+                Console.WriteLine(people[i].Sex);
+            }
+        }
+    }
+}
